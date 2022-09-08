56cf5bd8cb83744a374bb5857fd7b7e011d34b23
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-09-08 15:48:07 -0300

    service: ofertas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { OfertasService } from './ofertas.service';
 
 @NgModule({
   declarations: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'app/ofertas.service';
+import { Ofertas } from '../shared/Ofertas.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+  ofertas: Ofertas[]
 
-  constructor() { }
+  constructor(public ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas()
+    console.log(this.ofertas)
   }
 
 }
