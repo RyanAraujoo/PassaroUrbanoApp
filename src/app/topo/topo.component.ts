288c3f0bb8282b80c9621b85201f3a169c060d0d
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-09-23 19:50:15 -0300

    Observable: Subject

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { OfertasService } from 'app/ofertas.service';
 import { Ofertas } from 'app/shared/Ofertas.model';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
+// import 'rxjs/add/operator/of'
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+
+// import { of } from 'rxjs';
 
 @Component({
   selector: 'app-topo',
@@ -11,24 +19,30 @@ import { Observable } from 'rxjs/Observable';
 })
 export class TopoComponent implements OnInit {
   ofertasTo: Observable<Ofertas[]> | undefined;
+  subjectOfertas: Subject<string> = new Subject<string>()
 
-  constructor(private ofertasService: OfertasService) { 
-    
-  }
+  constructor(private ofertasService: OfertasService) {} 
 
   ngOnInit() {
+    this.ofertasTo = this.subjectOfertas // retorna a oferta []
+    .distinctUntilChanged() // se o event ( parametro ) for o mesmo, não trazer. 
+    .debounceTime(2000) // 2 segundos para realizar a ação do Subject
+    .switchMap((event: string) => { // metodo de fazer a observaçao
+      //  if (event.trim() === '') return of<Ofertas[]>([])
+        console.log("requisição http")
+        return this.ofertasService.getOfertaToPesquisa(event)
+    })
 
+    this.ofertasTo.subscribe((res: Ofertas[]) => { // metodo de ser observado
+      console.log(res)
+    }) 
   }
 
 
-  InputValueCaractere(event: string) { this.ofertasTo = this.ofertasService.getOfertaToPesquisa(event)
-  
-    this.ofertasTo.subscribe(
-      (res: Ofertas[] ) => { console.log(res )},
-      (error: any) => {console.log("error: " + error)},
-      // evento de conclusão a cada observação!
-      () => {}
-  )
+  InputValueCaractere(event: string) { 
+    console.log("pedido feito: " + event)
+    // subjectOferta aceite esse parametro e obserse o mais recente
+      this.subjectOfertas.next(event)
   }
   
   
