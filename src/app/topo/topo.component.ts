269ff2b6000d89287fa560993fad4e9f8dbb2cb8
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-09-24 10:19:49 -0300

    observables: template html

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,14 +3,7 @@ import { OfertasService } from 'app/ofertas.service';
 import { Ofertas } from 'app/shared/Ofertas.model';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
-
-// import 'rxjs/add/operator/of'
-import 'rxjs/add/operator/switchMap'
-import 'rxjs/add/operator/debounceTime'
-import 'rxjs/add/operator/distinctUntilChanged'
-
-// import { of } from 'rxjs';
-
+import '../util/rxjs-exports'
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
@@ -19,6 +12,7 @@ import 'rxjs/add/operator/distinctUntilChanged'
 })
 export class TopoComponent implements OnInit {
   ofertasTo: Observable<Ofertas[]> | undefined;
+  ofertas: Ofertas[]
   subjectOfertas: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) {} 
@@ -32,9 +26,13 @@ export class TopoComponent implements OnInit {
         console.log("requisição http")
         return this.ofertasService.getOfertaToPesquisa(event)
     })
-
+    .catch((error: any) => {  // caso, o switchMap falhe na obsrvação 
+        console.log(error)
+        return Observable.of<Ofertas[]>([])
+    })
     this.ofertasTo.subscribe((res: Ofertas[]) => { // metodo de ser observado
       console.log(res)
+      this.ofertas = res
     }) 
   }
 
@@ -44,6 +42,4 @@ export class TopoComponent implements OnInit {
     // subjectOferta aceite esse parametro e obserse o mais recente
       this.subjectOfertas.next(event)
   }
-  
-  
 }
