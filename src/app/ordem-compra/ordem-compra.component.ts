cad8f14c8e79743584f79d34146b7646decfd28c
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-10-06 11:13:53 -0300

    git refactory: template form

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,164 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { OrdemCompraService } from 'app/ordem-compra.service';
-import { Oferta } from 'app/shared/oferta.model';
-import { OfertasService } from './../ofertas.services';
-import { ordemCompra } from './../shared/ordem-compra.model';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OrdemCompraService } from '../ordem-compra.service'
+import { NgForm } from '@angular/forms';
+import { ordemCompra } from 'app/shared/ordem-compra.model';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [OfertasService,OrdemCompraService]
+  providers: [ OrdemCompraService ]
 })
-export class OrdemCompraComponent implements OnInit, OnDestroy {
-  public endereco: string = ''
-  public numero: string = ''
-  public complemento: string = ''
-  public formaPagamento: string = ''
+export class OrdemCompraComponent implements OnInit {
+  @ViewChild('formulario') public f: NgForm
+  public idCompra: number
 
-  public validaEndereco: boolean
-  public validaNumero: boolean
-  public validaComplemento: boolean
-  public validaFormaDePagamento: boolean
-
-  public validaTipoPrimitivoEndereco: boolean = true
-  public validaTipoPrimitivoNumero: boolean = true
-  public validaTipoPrimitivoComplemento: boolean = true
-  public validaTipoPrimitivoFormaDePagamento: boolean = true
-
-  public botaoConfirmarCompra: string = 'disabled'
-  public valorIncrementalOferta: number = 1
-  public ofertas: Oferta
-  public ofertasList: Oferta[] = new Array<Oferta>()
-  public pedido: ordemCompra = new ordemCompra('','','','')
-  public totalValorOfertas: number
-
-  public idPedidoCompra: number
-
-  constructor(
-    private route: ActivatedRoute,
-    private ofertasService: OfertasService,
-    private ordemCompraService: OrdemCompraService) {}
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-   this.route.params.subscribe((parametros: Params) => {
-     this.ofertasService.getOfertaPorId(parametros.id)
-      .then((res: Oferta) => {
-           this.ofertas = res
-           this.ofertasList.push(this.ofertas)
-           this.totalValorOfertas = this.atualizarValorTotal()
-           sessionStorage.setItem('carrinhoOfertas',JSON.stringify(this.ofertasList))
-      })
-    })
-  }
-
-  ngOnDestroy(): void {
 
   }
-
-  atualizarValorTotal(): number {
-    let x: Oferta
-    let valorTotal: number = 0
-      for (x of this.ofertasList) {
-          valorTotal += x.valor
-      }
-      return valorTotal
-  }
-
   confirmarCompra() {
-    this.pedido.endereco = this.endereco
-    this.pedido.complemento = this.complemento
-    this.pedido.numero = this.numero
-    this.pedido.formaDePagamento = this.formaPagamento
-      this.ordemCompraService.efetivarCompra(this.pedido).subscribe((res: ordemCompra) => {
-        this.idPedidoCompra = res.id
-      })
-
-
+    this.ordemCompraService.efetivarCompra(this.f.value).subscribe(
+    (res: ordemCompra) => {
+      console.log("Pedido feito com sucesso!")
+      this.idCompra = res.id },
+    (error: any) => { throw new Error(`Infelizmente! não conseguimos efetivar sua compra! ${error}`)}
+    )
   }
-
-  incrementando() {
-      this.valorIncrementalOferta++
-
+  botaoConfirmarEstadoDisabled(): string {
+     return this.f.invalid == true ? 'disabled' : ''
   }
-
-  decrementando() {
-    if (this.valorIncrementalOferta == 0) { return;}
-    this.valorIncrementalOferta--
-  }
-
-  public atualizaEndereco(endereco: string): void {
-   this.endereco = endereco
-
-    this.validaTipoPrimitivoEndereco = false
-
-   if (this.endereco.length > 3) {
-    this.validaEndereco = true
-  } else {
-    this.validaEndereco = false
-  }
-
-  if (this.endereco.length == 0) this.validaTipoPrimitivoEndereco = true
-  this.verificarValidacaoDosCampos()
-  }
-
-  isNumber(n: any) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-  public atualizaNumero(numero: string):void {
-    this.numero = numero
-
-    this.validaTipoPrimitivoNumero = false
-
-   if (this.isNumber(numero)) {
-    this.validaNumero = true
-  } else {
-    this.validaNumero = false
-  }
-
-  if (this.numero.length == 0) this.validaTipoPrimitivoNumero = true
-  this.verificarValidacaoDosCampos()
-
-  }
-
-  public atualizaComplemento(complemento: string): void {
-    this.complemento = complemento
-
-    this.validaTipoPrimitivoComplemento = false
-
-   if (this.complemento.length > 3) {
-    this.validaComplemento = true
-  } else {
-    this.validaComplemento = false
-  }
-
-  if (this.complemento.length == 0) this.validaTipoPrimitivoComplemento = true
-  this.verificarValidacaoDosCampos()
-  }
-
-  public atualizaFormaPagamento(formaPagamento: string):void {
-    this.formaPagamento = formaPagamento
-
-    if (this.formaPagamento !== '') {
-      this.validaFormaDePagamento = true
-      this.validaTipoPrimitivoFormaDePagamento = false
-    } else {
-      this.validaFormaDePagamento = false
-    }
-    this.verificarValidacaoDosCampos()
-  }
-
-    verificarValidacaoDosCampos (): boolean {
-      if (this.validaNumero === true && this.validaComplemento === true && this.validaFormaDePagamento === true && this.validaEndereco === true) {
-          this.botaoConfirmarCompra = ''
-         return true
-      } else {
-        this.botaoConfirmarCompra = 'disabled'
-        return false
-      }
-    }
-
 }
